0d3d10f680c83d39ed0e91137e19d777fa753c19
Demyan Gorchakov <Damian456@mail.ru>
2023-01-03 12:03:38 +0300

    Проект ДЗ 2

---
+++ Profil/Profil/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profil
+//
+//  Created by Демьян Горчаков on 03.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
